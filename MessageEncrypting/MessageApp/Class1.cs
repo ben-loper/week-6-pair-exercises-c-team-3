8205770e71384dd18059e42acacd6349e5bfae33
terriulloa <terri.ulloa7922@gmail.com>
2019-02-20 11:30:35 -0500

    Structure Complete

---
+++ MessageEncrypting/MessageApp/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MessageApp
+{
+    public class Class1
+    {
+    }
+}
---
+++ MessageEncrypting/MessageAppCLI/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MessageAppCLI
+{
+    public class Program
+    {
+    }
+}
